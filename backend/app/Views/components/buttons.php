c8b66ebb3e33685bafe654da4fdf59de1d6482aa
Joshua Ivan Reyes <joshuaivanp.reyes@gmail.com>
2025-10-08 18:02:34 +0800

    feat (components) : added components

---
+++ backend/app/Views/components/buttons.php
@@ -0,0 +1,4 @@
+<button class="btn btn-primary">Primary</button>
+<button class="btn btn-secondary">Secondary</button>
+<button class="btn btn-border">Border</button>
+<button class="btn btn-disabled" disabled>Disabled</button>
