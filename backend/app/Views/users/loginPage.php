da11deee45f734489c90fdf23418ece8669d6a36
Joshua Ivan Reyes <joshuaivanp.reyes@gmail.com>
2025-10-09 14:39:02 +0800

    feat (loginPage) : aded upgraded login

--- backend/app/Views/users/loginPage.php
+++ backend/app/Views/users/loginPage.php
@@ -9,7 +9,11 @@
       margin: 0;
       padding: 0;
       color: #fff;
-      background-color: #222; /* Dark theme */
+      background-color: #111; /* Dark theme */
+      background-image: url('/images/coffee_login.jpg');
+      background-size: cover; /* make image cover the whole page */
+    background-repeat: no-repeat; /* prevent tiling */
+    background-position: center; /* center the image */
       display: flex;
       align-items: center;
       justify-content: center;
@@ -17,10 +21,10 @@
     }
 
     .form-container {
-      background-color: #333; /* Dark card background */
+      background-color: rgba(129, 102, 67, 0.8); /* Dark card background */
       padding: 30px;
       border-radius: 10px;
-      box-shadow: 0px 2px 8px rgba(0,0,0,0.6);
+      box-shadow: 0px 2px 8px rgba(255, 255, 255, 0.6);
       width: 350px;
       text-align: center;
     }
@@ -30,21 +34,23 @@
       color: #D2B48C; /* Coffee gold */
       margin-bottom: 20px;
       text-transform: uppercase;
+      font-weight: bold;
     }
 
     label {
       display: block;
       margin: 10px 0 5px;
       text-align: left;
+      font-family: 'Pacifico', cursive;
     }
 
     input {
-      width: 100%;
+      width: 90%;
       padding: 10px;
       margin-bottom: 15px;
       border: none;
       border-radius: 5px;
-      background-color: #444;
+      background-color: #c19a6b;
       color: #fff;
     }
 
@@ -56,17 +62,20 @@
       width: 100%;
       padding: 12px;
       border: none;
-      border-radius: 5px;
+      border-radius: 8px;
       background-color: #D2B48C;
       color: #222;
       font-weight: bold;
       font-size: 1rem;
       cursor: pointer;
       transition: 0.3s;
+      box-shadow: 0px 2px 6px rgba(255, 255, 255, 1);
     }
 
     button:hover {
-      background-color: #c2a06f;
+      background-color: #c19a6b;
+      color: white;
+      box-shadow: 0px 4px 10px rgba(134, 106, 69, 0.6)
     }
   </style>
 </head>
